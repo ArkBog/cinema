84c4d1de2e540ac44a9b55e22bb6dacb7d9141d9
ArkBog <arkadiuszbogdanski1@gmail.com>
2024-04-13 18:09:17 +0200

    modified navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Menu } from "@/interfaces";
 import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const menu: Menu[] = [
   {
@@ -22,9 +24,18 @@ const menu: Menu[] = [
 ];
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className="flex w-full">
-      <div>
+    <div
+      className="flex w-full relative"
+    >
+      <button className="absolute top-0 right-0 z-30 p-2" onClick={toggleShowMenu}><GiHamburgerMenu /></button>
+      <div style={{ display: `${showMenu ? "flex" : "none"}` }} className="flex-col w-full text-center absolute top-0 left-0 bg-primary-color/20">
         {menu.map((e, i) => {
           return (
             <Link key={i} href={e.href} className="p-2">
